c9e593ef3a57f79a0a6fe73f1eff663d1ab82906
Joi <37125335+joicodes@users.noreply.github.com>
2018-11-14 12:10:32 -0500

    Updated semant

--- semant.ml
+++ semant.ml
@@ -71,4 +71,95 @@ let built_in_functions =
   in
 
   (* Collect all function names into one symbol table *)
-  let function_decls = List.fold_left add_func built_in_decls functions
+  let function_decls = List.fold_left add_func built_in_functions functions
+
+  in
+
+  (* Return a function from our symbol table *)
+  let find_func s = 
+    (* Checks to see if function is in function declaration map *)
+    try StringMap.find s function_decls
+    (* Throws an error if not found *)
+    with Not_found -> raise (Failure ("Function undefined " ^ s))
+
+  in
+
+  let _ = function_decl "main" in (* Ensure "main" is defined *)
+
+  let check_function func =
+    (* Make sure no formals or locals already defined  *)
+    dups "formal" func.parameters;
+    dups "local" func.local_variables;
+
+
+    (* Build local symbol table of variables for this function *)
+    let symbol_table = List.fold_left (fun m (ty, name) -> StringMap.add name ty m)
+	                StringMap.empty (globals @ func.parameters )
+    in
+
+    (* Return a variable from our local symbol table *)
+    let type_of_identifier s =
+      try StringMap.find s symbol_table
+      with Not_found -> raise (Failure ("Variable is undeclared " ^ s))
+
+    in
+
+    let check_assign lvaluet rvaluet err =
+       if lvaluet = rvaluet then lvaluet else raise (Failure err)
+    in   
+
+    (* Return a semantically-checked expression, i.e., with a type *)
+    let rec expr = function
+        Literal  l -> (Int, SLiteral l)
+      | Fliteral l -> (Float, SFliteral l)
+      | BoolLit l  -> (Bool, SBoolLit l)
+      | Noexpr     -> (Void, SNoexpr)
+      | Id s       -> (type_of_identifier s, SId s)
+      | Assign(var, e) as ex -> 
+          let lt = type_of_identifier var
+          and (rt, e') = expr e in
+          let err = "illegal assignment " ^ string_of_typ lt ^ " = " ^ 
+            string_of_typ rt ^ " in " ^ string_of_expr ex
+          in (check_assign lt rt err, SAssign(var, (rt, e')))
+      | Unop(op, e) as ex -> 
+          let (t, e') = expr e in
+          let ty = match op with
+            Neg when t = Int || t = Float -> t
+          | Not when t = Bool -> Bool
+          | _ -> raise (Failure ("illegal unary operator " ^ 
+                                 string_of_uop op ^ string_of_typ t ^
+                                 " in " ^ string_of_expr ex))
+          in (ty, SUnop(op, (t, e')))
+      | Binop(e1, op, e2) as e -> 
+          let (t1, e1') = expr e1 
+          and (t2, e2') = expr e2 in
+          (* All binary operators require operands of the same type *)
+          let same = t1 = t2 in
+          (* Determine expression type based on operator and operand types *)
+          let ty = match op with
+            Add | Sub | Mult | Div when same && t1 = Int   -> Int
+          | Add | Sub | Mult | Div when same && t1 = Float -> Float
+          | Equal | Neq            when same               -> Bool
+          | Less | Leq | Greater | Geq
+                     when same && (t1 = Int || t1 = Float) -> Bool
+          | And | Or when same && t1 = Bool -> Bool
+          | _ -> raise (
+	      Failure ("illegal binary operator " ^
+                       string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+                       string_of_typ t2 ^ " in " ^ string_of_expr e))
+          in (ty, SBinop((t1, e1'), op, (t2, e2')))
+      | Call(fname, args) as call -> 
+          let fd = find_func fname in
+          let param_length = List.length fd.formals in
+          if List.length args != param_length then
+            raise (Failure ("expecting " ^ string_of_int param_length ^ 
+                            " arguments in " ^ string_of_expr call))
+          else let check_call (ft, _) e = 
+            let (et, e') = expr e in 
+            let err = "illegal argument found " ^ string_of_typ et ^
+              " expected " ^ string_of_typ ft ^ " in " ^ string_of_expr e
+            in (check_assign ft et err, e')
+          in 
+          let args' = List.map2 check_call fd.formals args
+          in (fd.typ, SCall(fname, args'))
+    in
