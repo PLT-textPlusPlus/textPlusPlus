89d3559e5a1798a44d3efa38053e10ffc2d6ea7a
Joi Anderson <joicodes@gmail.com>
2018-11-25 15:00:46 -0500

    commented out SFloat

--- codegen.ml
+++ codegen.ml
@@ -102,7 +102,7 @@ let hello_func : L.llvalue =
     let rec expr builder ((_, e) : sexpr) = match e with
 	SNumber i  -> L.const_int i32_t i
       | SBoolean b  -> L.const_int bool_t (if b then 1 else 0)
-      | SFloat l -> L.const_float_of_string float_t l
+      (*| SFloat l -> L.const_float_of_string float_t l *)
       | SString l -> L.build_global_stringptr s "string" llbuilder
       | SNoexpr     -> L.const_int i32_t 0
       | SId s       -> L.build_load (lookup s) s builder
